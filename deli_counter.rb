8a502c891eda82c396649c9a029d0d5eaa189a6a
Pan Cheng <pchengeducation@hotmail.com>
2018-07-05 03:52:52 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,6 +18,7 @@ puts "The line is currently: #{new_list.join" "}"
 def take_a_number (katz_deli, wish_join)
   if katz_deli.length <= 0
     katz_deli.push(wish_join)
+    puts "Welcome, #{katz_deli[0]}. You are number 1 in line."
   else
   katz_deli.each do |name|
   line_number = katz_deli.index(name) + 1
