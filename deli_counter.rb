92c7ef17943e7d1a2f08cc8b014353c8e9e29ecb
Pan Cheng <pchengeducation@hotmail.com>
2018-07-05 02:27:28 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,8 @@ def line(katz_deli)
     return puts "The line is currently empty."
   else
 katz_deli.each do |name|
-  puts "Welcome, #{name}. You are number #{katz_deli.index(name)} in line."
+  line_number = katz_deli.index(name) + 1
+  puts "Welcome, #{name}. You are number #{line_number} in line."
 end
 end
 end
